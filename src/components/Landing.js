4336e148e009ae65ab7395bf97f77c398e669cb5
Matt Pedersen <mpedersen2054@gmail.com>
2018-01-29 21:52:57 -0500

    redo Landing page. write code for half the page, entering the players name and game speed. the other half will be the top X scores

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -20,8 +20,15 @@ class Landing extends Component {
         this.setState({ name: e.target.value })
     }
 
-    changeSelector(e) {
-        console.log('changing slector', e)
+    changeSelector(idx) {
+        this.setState(prevState => {
+            return {
+                gameSpeeds: prevState.gameSpeeds.map(speed => {
+                    speed.selected = speed.id == idx ? true : false
+                    return speed
+                })
+            }
+        })
     }
 
     validateInfo(idx) {
@@ -31,29 +38,45 @@ class Landing extends Component {
     render() {
         return(
             <div className="landing-container">
-                <div className="well">
-                    <h1>Snake Game!</h1>
-
+                <h1>Snake Game!</h1>
+                <div className="player-info-container">
+                    <div className="sub-head">Start a new game</div>
                     <div className="player-info">
-                        <input
-                            onChange={this.changeName}
-                            value={this.state.name}
-                            type="text" />
 
-                        <div className="selectors">
-                            {this.state.gameSpeeds.map(speed => {
-                                return(
-                                    <div
-                                        className="selector"
-                                        key={speed.id}
-                                        onClick={(e) => this.changeSelector(speed.id)}>
-                                            {speed.text}
-                                    </div>
-                                )
-                            })}
+                        <div className="form-section">
+                            <div className="label">Your name</div>
+                            <input
+                                onChange={this.changeName}
+                                value={this.state.name}
+                                type="text" />
                         </div>
 
-                        <button onClick={this.validateInfo}>Start</button>
+                        <div className="form-section">
+                            <div className="label">Game speed</div>
+                            <div className="selectors">
+                                {this.state.gameSpeeds.map(speed => {
+                                    return(
+                                        <div
+                                            className={`selector ${speed.selected ? 'selected' : ''} ${speed.id == 1 ? 'medium' : ''}`}
+                                            key={speed.id}
+                                            onClick={(e) => this.changeSelector(speed.id)}>
+                                                {speed.text}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+
+                        <div className="form-section">
+                            <button onClick={this.validateInfo}>Start</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="scores-container">
+                    <div className="sub-head">Top scores</div>
+                    <div className="scores">
+                        hello scores
                     </div>
                 </div>
             </div>
