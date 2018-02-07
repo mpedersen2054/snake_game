80967be9a40dbe85e7ee520fe2f9762e07877679
Matt Pedersen <mpedersen2054@gmail.com>
2018-02-07 17:10:27 -0500

    update styles on Landing

--- prodServer.js
+++ prodServer.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 require('./server/config/routes')(app)
 
 app.use(express.static(path.join(__dirname, 'client', 'dist')))
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html')))
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html')))
 
 app.listen(PORT, 'localhost', (err) => {
     if (err) {
